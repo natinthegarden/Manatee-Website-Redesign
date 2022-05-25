4ff5716f89d5eb6ff73f95677ee6db14d56f75e8
Natalya_Naser <nat.naser@gmail.com>
2022-05-24 23:15:59 -0400

    added javascript for hamburger

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function () {
-    myFunction()
-};
-
-// Get the navbar
-var navbar = document.getElementById("navbar");
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
+    console.log("im alive")
+    let navLinks = document.getElementById('navMenu');
+    if (navLinks.style.display === "none") {
+        navLinks.style.display = "block";
     } else {
-        navbar.classList.remove("sticky");
+        navLinks.style.display = "none";
+    }
+}
+
+
+
+
+
+
+function validateForm() {
+    let x = document.forms["myForm"]["fname"].value;
+    if (x == "") {
+        alert("Name must be filled out");
+        return false;
     }
 }
