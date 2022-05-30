b9ca9042b36dd142bc45d753c8373b39743a5048
Natalya_Naser <nat.naser@gmail.com>
2022-05-30 11:26:53 -0400

    simplified form and attempted to validate

--- script.js
+++ script.js
@@ -7,16 +7,51 @@ function myFunction() {
         navLinks.style.display = "none";
     }
 }
+//
+const form = document.getElementById('form');
+const manateeName = document.getElementById('manateeName');
+const email = document.getElementById('email');
+const yourName = document.getElementById('yourName');
 
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
 
+    checkInputs();
 
+});
 
+function checkInputs() {
+    const manateeNameValue = manateeName.value.trim();
+    const emailValue = email.value.trim();
+    const yourNameValue = yourName.value.trim();
 
+    if (manateeName === '') {
+        //showerror
+        //add error class
+        form.small.display === display;
+        setErrorFor(manateeName, 'Manatee Name cannot be blank')
+    } else {
+        //addsucces class
+        setSuccessFor(username);
+    }
+}
+
+function setErrorFor(input, message) {
+    const formControl = input.parentElement; //.formcontrol
+    const small = formControl.querySelector('small');
+
+    small.innerText = message;
+
+    formControl.className = 'form.control error';
+}
+//
 
 function validateForm() {
-    let x = document.forms["myForm"]["email"].value;
-    if (x == "") {
-        alert("Email must be filled out");
+    let adoptionForm = document.forms["myForm"]["fname"].value;
+    if (adoptionForm == "") {
+        alert("Name must be filled out");
         return false;
+    } else {
+        alert("Thank you for naming");
     }
 }
